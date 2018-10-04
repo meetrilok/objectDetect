826f9d12ac91e5dc628d6c417c982e4cea850b43
Trilok <mtrilokchowdary@ymail.com>
2018-10-04 22:51:51 +0530

    Update views.py

--- classify_image/views.py
+++ classify_image/views.py
@@ -1,5 +1,9 @@
 import io
 import os
+import json
+import glob
+import cv2   
+import numpy as np
 
 from base64 import b64decode
 import tensorflow as tf
@@ -16,6 +20,8 @@ TF_GRAPH = "{base_path}/inception_model/graph.pb".format(
 TF_LABELS = "{base_path}/inception_model/labels.txt".format(
     base_path=os.path.abspath(os.path.dirname(__file__)))
 
+
+impath=""
 
 def load_graph():
     sess = tf.Session()
@@ -43,10 +49,12 @@ def classify_api(request):
             image_bytes = image_request.read()
             image = Image.open(io.BytesIO(image_bytes))
             image.save(tmp_f, image.format)
+            impath=tmp_f
         elif request.POST.get("image64", None) is not None:
             base64_data = request.POST.get("image64", None).split(',', 1)[1]
             plain_data = b64decode(base64_data)
             tmp_f.write(plain_data)
+            impath=tmp_f
 
         classify_result = tf_classify(tmp_f, int(request.POST.get('k', MAX_K)))
         tmp_f.close()
@@ -63,7 +71,7 @@ def classify_api(request):
 def classify(request):
     blue_color='yes'
     context={
-        'blue_color':blue_color
+        'blue_color':checkBlueColor(impath)
         }
     return render(request, 'classify.html', context)
 
@@ -83,3 +91,49 @@ def tf_classify(image_file, k=MAX_K):
         result.append([label_string, score])
 
     return result
+
+
+def checkBlueColor(imgPath):
+    frame = cv2.imread(imgPath)
+    
+    img = frame
+
+    #convert BGR to HSV
+    hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    #define Range of colors
+    blue_lower=np.array([99,115,150],np.uint8)
+    blue_upper=np.array([110,255,255],np.uint8)
+
+
+    blue=cv2.inRange(hsv,blue_lower,blue_upper)
+    kernal = np.ones((5 ,5), "uint8")
+    #print("kernal done")
+
+    #find blue contours
+    blue=cv2.dilate(blue,kernal)
+    res1=cv2.bitwise_and(img, img, mask = blue)
+    (_,contours,hierarchy)=cv2.findContours(blue,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    
+    count=0
+    ImgBlueStat="no"
+    #print file path for no blue detect. 
+    if(contours==[]):
+        print(imgPath)
+        return "None"
+    else:
+        for pic, contour in enumerate(contours):
+            
+            area = cv2.contourArea(contour)
+            
+            if(area>300):
+                
+                if(count==0):
+
+                    #print fpath and yes only once.
+                    #print(imgPath, "yes")
+                    count=count+1
+                    return "Yes"
+                
+                x,y,w,h = cv2.boundingRect(contour)	
+                img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+                cv2.putText(img,"Blue color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (150,0,0))
